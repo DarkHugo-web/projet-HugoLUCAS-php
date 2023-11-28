c91492271577254d2f673e6339e59cd266fb0b38
DarkHugo-web <hugo.lucas@efrei.net>
2023-11-28 14:56:08 +0100

    add backend

---
+++ backend/pdoConnect.php
@@ -0,0 +1,34 @@
+<?php
+
+try{
+    $dsn = 'mysql:host=51.158.59.186;dbname=H.L;charset=utf8mb4;port=14301';
+    $username = 'phppex';
+    $password = 'Supermotdepasse!42';
+    $pdo = new PDO($dsn, $username, $password);
+    echo "Connected successfully";
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    } 
+
+
+    $sql = "SELECT * FROM customer WHERE id=1";
+    $stmt = $pdo-> query($sql);
+    $data = $stmt->fetch();
+    var_dump($data);
+ 
+/*$createTableQuery = "
+    CREATE TABLE students (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        nationality VARCHAR(255) NOT NULL
+    )
+";
+
+$pdo -> exec($createTableQuery);*/
+
+
+
+//$stmt = $pdo->query("SELECT * FROM students");
+
+//$stmt = $pdo->prepare("UPDATE students SET name = :name WHERE id = :id");
+//$stmt->execute(['name' => 'Jane Smith', 'id']);
