7b8b2195fb19bf0ebd9936751204833209c91451
DarkHugo-web <hugo.lucas@efrei.net>
2023-11-29 13:53:17 +0100

    add files

---
+++ controller/index.php
@@ -0,0 +1,17 @@
+<?php require 'header.php';?>
+<body>
+
+<?php require_once 'modal.php'?>
+
+</body>
+<?php require_once 'footer.php'?>
+
+
+<script>
+modal = document.getElementById("modal") 
+console.log(modal)
+modal.style.display ="none"   
+</script>
+
+</html>
+
